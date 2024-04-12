6ec167da61174bc778c6e99107e6a20f80fc3f98
Amir Allahdadian <amir.allahdadian@gmail.com>
2024-04-12 11:36:16 +0330

    added header icons and page logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  saxShopOutline,
+  sax3dcubeOutline,
+  saxShoppingCartOutline,
+  saxInformationOutline,
+  saxUserOutline,
+  saxLoginOutline,
+} from '@ng-icons/iconsax/outline';
+import { saxShoppingBagBold } from '@ng-icons/iconsax/bold';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterOutlet, RouterLinkActive],
+  imports: [RouterOutlet, RouterOutlet, RouterLinkActive, NgIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  viewProviders: [
+    provideIcons({
+      saxShopOutline,
+      saxShoppingBagBold,
+      sax3dcubeOutline,
+      saxShoppingCartOutline,
+      saxInformationOutline,
+      saxUserOutline,
+      saxLoginOutline,
+    }),
+  ],
 })
 export class AppComponent {
   title = 'Authentication';
